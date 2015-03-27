2498f8d2c451dc63855c98bd3190830875a17f80
Fernando Fdz. Quirós <fernanezquiros@gmail.com>
2015-03-27 09:03:12 +0100

    Update ASPtest5.js

--- ASPtest5.js
+++ ASPtest5.js
@@ -1,20 +1,20 @@
 casper.test.begin('Comprobar pagina ASPgems - Test5 - Contacto', 5, function suite(test) {
     
   	casper.start('http://aspgems.com/', function() {
-    	casper.click("a[href='/contacto']");
+    		casper.click("a[href='/contacto']"); //hacer click en contactos
   	});
 
   	casper.then(function(){		
  	  	test.assertUrlMatch('http://aspgems.com/contacto', 'Direccion URL correcta'); //comprobar la direccion URL
- 	  	test.assertExists('.webform-client-form');
- 	  	test.assertExists('.block-title');
- 	  	test.assertExists('.group-left');
- 	  	test.assertExists('.group-right');
+ 	  	test.assertExists('.webform-client-form'); //comprobar el formulario para enviar un e-mail
+ 	  	test.assertExists('.block-title'); //comprobar titulo encuentranos
+ 	  	test.assertExists('.group-left'); //comprobar direccion de la empresa
+ 	  	test.assertExists('.group-right'); //comprobar mapa
 	});
 
   	casper.run(function() {
-    	casper.echo("Test finalizado"); //mensaje
-    	test.done();
+    		casper.echo("Test finalizado"); //mensaje
+    		test.done();
   	});
 
 });
