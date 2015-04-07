45158af48ab6b8f9ae320dabd07bb828dc46eb50
Fernando Fdz. Quirós <fernanezquiros@gmail.com>
2015-04-07 10:45:58 +0200

    Update ASPtest8.js

--- ASPtest8.js
+++ ASPtest8.js
@@ -10,7 +10,7 @@ casper.test.begin('Comprobar pagina ASPgems - Test8 - Presupuesto', 6, function 
  	  	test.assertUrlMatch('http://aspgems.com/presupuesto', 'Direccion URL correcta'); //comprobar la direccion URL
  	  	test.assertExists(x('//*[@class="view-header"]'), 'the element exists view-header'); //comprobar el subtitulo
  	  	test.assertExists(x('//*[@class="view-filters"]'), 'the element exists view-filters'); //comprobar cuadro ¿Dispone de presupuesto?
- 	  	test.assertExists(x('//*[@class="description"]'), 'the element exists description'); //comprobar lista de post
+ 	  	test.assertExists(x('//*[@class="description"]'), 'the element exists description'); //comprobar "Podemos ayudarte......"
  	  	test.assertExists(x('//*[@class="view-content"]'), 'the element exists view-content'); //comprobar titulo de tipos de solucciones
  	  	test.assertExists(x('//*[@id="block-webform-client-block-1122"]'), 'the element exists block-webform-client-block-1122'); //comprobar lista de tipos de solucciones
 	});
