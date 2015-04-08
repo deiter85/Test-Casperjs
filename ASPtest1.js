ca5cc71a7edf4a53072e5335ac9e4116f3c8079a
Fernando <fernanezquiros@gmail.com>
2015-04-08 09:43:25 +0200

    Update

--- ASPtest1.js
+++ ASPtest1.js
@@ -1,6 +1,5 @@
 casper.test.begin('Comprobar pagina ASPgems - Test1', 11, function suite(test) {
     
-<<<<<<< HEAD
 	casper.start('https://aspgems.com/', function() {
 		this.echo(this.getCurrentUrl());
     	casper.viewport(1200,720); //modificar las dimensiones del navegador
@@ -17,23 +16,22 @@ casper.test.begin('Comprobar pagina ASPgems - Test1', 11, function suite(test) {
  		test.assertSelectorHasText('h1', 'trabajos'); //comprobar si el selector contiene un texto
   		test.assertTextExists('trabajos', 'La pagina contiene la palabra "trabajos"'); //comprobar que existe un texto
   	});
-=======
-  casper.start('https://aspgems.com/', function() {
-    casper.viewport(1200,720); //modificar las dimensiones del navegador
-    test.assertHttpStatus(200); //comprobar el codigo de estatus Http
-    test.assertTitle('ASPgems Soluciones Ágiles | Soluciones Ágiles', 'Titulo --> ' + this.getTitle()); //comprobar el titulo de la pagina
-    test.assertResourceExists('logo.png'); //comprobar recursos
-    test.assertElementCount('body', 1); //comprobar el numero de un elemento
-    test.assertExists('.wrapper'); //comprobar si existe un elemento
+  
+  	casper.start('https://aspgems.com/', function() {
+    	casper.viewport(1200,720); //modificar las dimensiones del navegador
+    	test.assertHttpStatus(200); //comprobar el codigo de estatus Http
+    	test.assertTitle('ASPgems Soluciones Ágiles | Soluciones Ágiles', 'Titulo --> ' + this.getTitle()); //comprobar el titulo de la pagina
+    	test.assertResourceExists('logo.png'); //comprobar recursos
+    	test.assertElementCount('body', 1); //comprobar el numero de un elemento
+    	test.assertExists('.wrapper'); //comprobar si existe un elemento
 	  casper.click("a[href='/trabajos']"); //hacer click en un hipervinculo
-  });
+  	});
 
-  casper.then(function(){		
+  	casper.then(function(){		
  	 	test.assertUrlMatch('https://aspgems.com/trabajos', 'Direccion URL correcta'); //comprobar la direccion URL
  	 	test.assertSelectorHasText('h1', 'trabajos'); //comprobar si el selector contiene un texto
-  	test.assertTextExists('trabajos', 'La pagina contiene la palabra "trabajos"'); //comprobar que existe un texto
-  });
->>>>>>> 8720115f391afa2257d12e742b2f6abbf0cff5e1
+  		test.assertTextExists('trabajos', 'La pagina contiene la palabra "trabajos"'); //comprobar que existe un texto
+  	});
 
   	casper.then(function(){	
    		casper.fill('form[action="/search/node"]', { custom_search_blocks_form_1: 'presupuesto' }, true); //rellena un formulario
--- ASPtest5.js
+++ ASPtest5.js
@@ -1,30 +1,18 @@
 casper.test.begin('Comprobar pagina ASPgems - Test5 - Contacto', 9, function suite(test) {
     
-<<<<<<< HEAD
-<<<<<<< HEAD
   	casper.start('https://aspgems.com/', function() {
     	casper.click("a[href='/contacto']"); //hacer click en contactos
   	});
 
   	casper.then(function(){		
  	  	test.assertUrlMatch('https://aspgems.com/contacto', 'Direccion URL correcta'); //comprobar la direccion URL
-=======
-  	casper.start('http://aspgems.com/', function() {
-=======
-  	casper.start('https://aspgems.com/', function() {
->>>>>>> 8720115f391afa2257d12e742b2f6abbf0cff5e1
-
     	casper.click("a[href='/contacto']"); //hacer click en contactos
-    		casper.click("a[href='/contacto']"); //hacer click en contactos
+    	casper.click("a[href='/contacto']"); //hacer click en contactos
   	});
 
   	casper.then(function(){		
-<<<<<<< HEAD
  	  	test.assertUrlMatch('http://aspgems.com/contacto', 'Direccion URL correcta'); //comprobar la direccion URL
->>>>>>> bf46cd249019317a21f46a1f1649a0187bcc005f
-=======
  	  	test.assertUrlMatch('https://aspgems.com/contacto', 'Direccion URL correcta'); //comprobar la direccion URL
->>>>>>> 8720115f391afa2257d12e742b2f6abbf0cff5e1
  	  	test.assertExists('.webform-client-form', 'the element exists webform-client-for'); //comprobar el formulario para enviar un e-mail
  	  	test.assertExists('.block-title', 'the element exists block-title'); //comprobar titulo encuentranos
  	  	test.assertExists('.group-left', 'the element exists group-left'); //comprobar direccion de la empresa
@@ -33,7 +21,7 @@ casper.test.begin('Comprobar pagina ASPgems - Test5 - Contacto', 9, function sui
  	  	test.assertExists('.block-title'); //comprobar titulo encuentranos
  	  	test.assertExists('.group-left'); //comprobar direccion de la empresa
  	  	test.assertExists('.group-right'); //comprobar mapa
-	});
+	  });
 
   	casper.run(function() {
     	casper.echo("Test finalizado"); //mensaje
