dcf09943ac9fe37f43737e1d938a265654a6e1d7
Fernando Fdz. Quirós <fernanezquiros@gmail.com>
2015-04-08 09:30:05 +0200

    Update ASPtest6.js

--- ASPtest6.js
+++ ASPtest6.js
@@ -2,12 +2,12 @@ var x = require('casper').selectXPath;
 
 casper.test.begin('Comprobar pagina ASPgems - Test6 - Blog', 8, function suite(test) {
     
-  	casper.start('http://aspgems.com/', function() {
+  	casper.start('https://aspgems.com/', function() {
     	casper.click("a[href='/blog']"); //hacer click en blog
   	});
 
   	casper.then(function(){		
- 	  	test.assertUrlMatch('http://aspgems.com/blog', 'Direccion URL correcta'); //comprobar la direccion URL
+ 	  	test.assertUrlMatch('https://aspgems.com/blog', 'Direccion URL correcta'); //comprobar la direccion URL
  	  	test.assertExists(x('//*[@id="page-title"]'), 'the element exists page-title'); //comprobar el titulo
  	  	test.assertExists(x('//*[@id="block-customblocks-block-subtitle-blog"]'), 'the element exists block-customblocks-block-subtitle-blog'); //comprobar el subtitulo
  	  	test.assertExists(x('//*[@id="block-system-main"]'), 'the element exists block-system-main'); //comprobar lista de post
