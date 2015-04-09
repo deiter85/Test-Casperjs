8790ecde0a7ea386acbfe21077e8b587c7491157
Fernando <fernanezquiros@gmail.com>
2015-04-09 09:15:35 +0200

    Update 09/04/2015

--- ASPtest5.js
+++ ASPtest5.js
@@ -1,11 +1,12 @@
-casper.test.begin('Comprobar pagina ASPgems - Test5 - Contacto', 9, function suite(test) {
+var x = require('casper').selectXPath;
+
+casper.test.begin('Comprobar pagina ASPgems - Test5 - Contacto', 11, function suite(test) {
     
 	casper.start('https://aspgems.com/', function() {
    		casper.click("a[href='/contacto']"); //hacer click en contactos
   	});
 
   	casper.then(function(){		
- 		test.assertUrlMatch('http://aspgems.com/contacto', 'Direccion URL correcta'); //comprobar la direccion URL
  		test.assertUrlMatch('https://aspgems.com/contacto', 'Direccion URL correcta'); //comprobar la direccion URL
  		test.assertExists('.webform-client-form', 'the element exists webform-client-for'); //comprobar el formulario para enviar un e-mail
  		test.assertExists('.block-title', 'the element exists block-title'); //comprobar titulo encuentranos
@@ -15,6 +16,8 @@ casper.test.begin('Comprobar pagina ASPgems - Test5 - Contacto', 9, function sui
  		test.assertExists('.block-title'); //comprobar titulo encuentranos
  		test.assertExists('.group-left'); //comprobar direccion de la empresa
  		test.assertExists('.group-right'); //comprobar mapa
+ 		test.assertExists(x('//*[@class="field field-name-title field-type-ds field-label-hidden view-mode-full"]'), 'the element exists field field-name-title field-type-ds field-label-hidden view-mode-full'); //comprobar titulo
+ 		test.assertExists(x('//*[@class="field field-name-body field-type-text-with-summary field-label-hidden view-mode-full"]'), 'the element exists field field-name-body field-type-text-with-summary field-label-hidden view-mode-full'); //comprobar subtitulo
   	});
 
   	casper.run(function() {
