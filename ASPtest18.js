40f7c20753e0c7a0587d9fcdcbfcc9b7ec21605c
Fernando <fernanezquiros@gmail.com>
2015-04-09 11:41:58 +0200

    Update 09/04/2014

--- ASPtest18.js
+++ ASPtest18.js
@@ -12,18 +12,18 @@ casper.test.begin('Comprobar pagina ASPgems - Test18 - Presupuesto-Aplicacion Ge
 
   	casper.then(function() {		
   	   	test.assertUrlMatch('https://aspgems.com/presupuesto/aplicacion-de-gestion-avanzada', 'Direccion URL correcta'); //comprobar la direccion URL
-  		  test.assertTitle('Aplicación de gestión avanzada | ASPgems Soluciones Ágiles', 'Titulo --> ' + this.getTitle()); //comprobar el titulo de la pagina
-  		  test.assertExists(x('//*[@id="page-title"]'), 'the element exists page-title'); //comprobar el titulo
-  		  test.assertExists(x('//a[@title="Volver a presupuestos" and @href="/presupuesto"]'), 'the element exists link-budget'); //comprobar link volver a presupuesto
-    	  test.assertExists(x('//*[@class="field field-name-field-project-type-description field-type-text-with-summary field-label-hidden view-mode-full"]'), 'the element exists field field-name-field-project-type-description field-type-text-with-summary field-label-hidden view-mode-full'); //comprobar texto de la pagina
+  		test.assertTitle('Aplicación de gestión avanzada | ASPgems Soluciones Ágiles', 'Titulo --> ' + this.getTitle()); //comprobar el titulo de la pagina
+  		test.assertExists(x('//*[@id="page-title"]'), 'the element exists page-title'); //comprobar el titulo
+  		test.assertExists(x('//a[@title="Volver a presupuestos" and @href="/presupuesto"]'), 'the element exists link-budget'); //comprobar link volver a presupuesto
+    	test.assertExists(x('//*[@class="field field-name-field-project-type-description field-type-text-with-summary field-label-hidden view-mode-full"]'), 'the element exists field field-name-field-project-type-description field-type-text-with-summary field-label-hidden view-mode-full'); //comprobar texto de la pagina
       	test.assertExists(x('//*[@class="field field-name-field-project-type-range field-type-image field-label-above view-mode-full"]'), 'the element exists field field-name-field-project-type-range field-type-image field-label-above view-mode-full'); //comprobar cuadro rango de precio            
       	test.assertExists(x('//*[@class="field field-name-field-project-type-time-range field-type-image field-label-above view-mode-full"]'), 'the element exists field field-name-field-project-type-time-range field-type-image field-label-above view-mode-full'); //comprobar cuadro rango de tiempo
       	test.assertExists(x('//*[@id="block-webform-client-block-1100"]'), 'the element exists block-webform-client-block-1100'); //comprobar formulario Solicitar presupuesto
    	});
 
    	casper.run(function() {
-   		  casper.echo("Test finalizado"); //mensaje
-   		  test.done();
+   		casper.echo("Test finalizado"); //mensaje
+   		test.done();
    	});
 
 });
