c089fd8c642056c0a0319e07fa0bfe820c1ac78e
Fernando <fernanezquiros@gmail.com>
2015-04-09 09:09:39 +0200

    Update 09/04/2015

--- ASPtest8.js
+++ ASPtest8.js
@@ -11,8 +11,8 @@ casper.test.begin('Comprobar pagina ASPgems - Test8 - Presupuesto', 6, function 
  	  	test.assertExists(x('//*[@class="view-header"]'), 'the element exists view-header'); //comprobar el subtitulo
  	  	test.assertExists(x('//*[@class="view-filters"]'), 'the element exists view-filters'); //comprobar cuadro ¿Dispone de presupuesto?
  	  	test.assertExists(x('//*[@class="description"]'), 'the element exists description'); //comprobar "Podemos ayudarte....."
- 	  	test.assertExists(x('//*[@class="view-content"]'), 'the element exists view-content'); //comprobar titulo de tipos de solucciones
- 	  	test.assertExists(x('//*[@id="block-webform-client-block-1122"]'), 'the element exists block-webform-client-block-1122'); //comprobar lista de tipos de solucciones
+ 	  	test.assertExists(x('//*[@class="item-list"]'), 'the element exists item-list'); //comprobar titulo de tipos de solucciones
+ 	  	test.assertExists(x('//*[@id="block-webform-client-block-1122"]'), 'the element exists block-webform-client-block-1122'); //comprobar no encuentras lo que buscas
 	});
 
   	casper.run(function() {
