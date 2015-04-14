b5819b55802c33c0e2c83f822082b8da2e38022f
Fernando <fernanezquiros@gmail.com>
2015-04-14 09:16:52 +0200

    Update 14/04/2015

--- ASPtest22.js
+++ ASPtest22.js
@@ -1,3 +1,7 @@
+var userName = ****; //Nombre de usuario
+
+var password = ****; //Contraseña
+
 var editor_allow = ["/node/add/culture",
 					"/node/add/employee",
 					"/node/add/company-logo",
@@ -15,42 +19,67 @@ var editor_allow = ["/node/add/culture",
 					"/node/add/forum",
 					"/node/add/what-we-do",
 					"/node/add/webform",
-					"/node/add/panel/choose-layout",
 					"/node/add/basic-page",
 					"/node/add/feed-item",
 					"/node/add/banner",
 					"/node/add/banner-first-clone",
 					"/node/add/banner-second-clone",
 					"/node/add/banner-third-clone",
-					"/node/add/asplab-project",];
-var userName = ****;
-var password = ****;
-var indice = 0;
+					"/node/add/asplab-project",]; //Nodos que se permiten con los permisos de este usuario
+
+var editor_disallow = ["/node/add/panel/choose-layout"]; //Nodos que no se permiten con los permisos de este usuario
+
+var indice = 0; //Contador
 
 casper.test.begin('Comprobar pagina ASPgems - Test22 - Http Status', 0, function suite(test) {   
-  casper.start('https://aspgems.com/user', function() {
+  
+  casper.start();
+  
+  casper.thenOpen('/user', function() {
+  	test.assertTitle('Cuenta de usuario | ASPgems Soluciones Ágiles'); 
     this.echo('⌚ Logging in....');
     this.fill('form[action="/user"]', { //Iniciar sesion
       	'name': userName,
       	'pass': password
     }, true);
   });
-  casper.then(function(){
-		test.assertTitle('editor | ASPgems Soluciones Ágiles', 'Authentication successful');
+  
+  casper.wait(1000, function() {});
+  
+  casper.then(function() {
+		test.assertTitle('editor | ASPgems Soluciones Ágiles', 'Authentication successful'); //Comprobar que se ha accedido correctamente
   });
-  for (i=0;i<editor_allow.length;i++) {     
-    casper.thenOpen('https://aspgems.com'+editor_allow[i], function() {
-      	this.echo('⌚ Opened the '+editor_allow[indice++]);
-      	this.echo(this.getTitle()); // mostrar titulo
-      	test.assertHttpStatus(200);	// comprobar estatus del http   
+  
+  for (i = 0; i < editor_allow.length; i++) {     
+    casper.thenOpen('https://aspgems.com' + editor_allow[i], function() {
+      	this.echo('⌚ Opened the ' + editor_allow[indice++]);
+      	test.assertHttpStatus(200);	// Comprobar estatus del http de los distintos nodos  
     });
   }
-  casper.then(function(){
-		casper.click("a[href='/user/logout']"); //cerrar sesion
-		this.echo('⌚ Log out');
+  
+  var indice2 = 0; //Contador
+  
+  for (i = 0; i < editor_disallow.length; i++) {     
+    casper.thenOpen('https://aspgems.com' + editor_disallow[i], function() {
+      	this.echo('⌚ Opened the ' + editor_disallow[indice2++]);
+      	test.assertHttpStatus(403);	// Comprobar estatus del http de los distintos nodos 
+    });
+  }
+  
+  casper.then(function() {
+		casper.click("a[href='/user/logout']"); //Cerrar sesion
+		this.echo('⌚ Log out....');
   });
+  
+  casper.wait(1000, function() {});
+  
+  casper.then(function() {
+		test.assertTitle('ASPgems Soluciones Ágiles | Soluciones Ágiles', 'Disconnection successful'); //Comprobar que se ha cerrado la sesion
+  });
+  
   casper.run(function() {
   		casper.echo('Test finalizado');
   		test.done();
   });
+
 });
