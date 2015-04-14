84ec54999120b92056eaaa089ddf1e8e8b836c20
Fernando <fernanezquiros@gmail.com>
2015-04-14 13:36:13 +0200

    Initial commit

--- AbbvieCampus/alive.js
+++ AbbvieCampus/alive.js
@@ -1,8 +1,17 @@
-casper.test.begin('Tests homepage and request new password page structure', 4, function suite(test) {
+var x = require('casper').selectXPath;
+
+casper.test.begin('Tests homepage and request new password page structure', 8, function suite(test) {
   casper.start('http://www.campusabbvie.com/user', function() {
     test.assertTitle('Inicio de sesión | AbbVie');
+  });
+  casper.then(function(){
     test.assertHttpStatus(200, 'Homepage was loaded successfully.');
+  });
+  casper.then(function(){
     test.assertExists('form#user-login', 'Login form is present.');
+  });
+  casper.then(function(){
+    test.assertExists(x('//*[@class="site-logo image-style-none"]'), 'Se encuentra el Logo'); //comprueba si esta el logo
   });
   casper.then(function(){
     test.assertExists('header.clearfix'); // comprueba el header
@@ -13,8 +22,12 @@ casper.test.begin('Tests homepage and request new password page structure', 4, f
   });
   casper.wait(1000, function() {});
   casper.then(function(){
-    test.assertTitle('Solicitar una contraseña | AbbVie'); 
+    test.assertTitle('Solicitar una contraseña | AbbVie');
+  });
+  casper.then(function(){ 
     test.assertHttpStatus(200, 'Request new password page was loaded successfully.');
+  });
+  casper.then(function(){
     test.assertExists('form#user-pass', 'Request new password form is present.');
   });
   casper.run(function() {
