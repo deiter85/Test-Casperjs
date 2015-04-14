2fdf0120bc93323d4bc68b520987588a1bf9494d
Fernando <fernanezquiros@gmail.com>
2015-04-14 13:12:54 +0200

    Initial commit

--- AbbvieCampus/alive.js
+++ AbbvieCampus/alive.js
@@ -1,11 +1,20 @@
 casper.test.begin('Tests homepage and request new password page structure', 4, function suite(test) {
-  casper.start();
-  casper.thenOpen('/user', function() {
+  casper.start('http://www.campusabbvie.com/user', function() {
+    test.assertTitle('Inicio de sesión | AbbVie');
     test.assertHttpStatus(200, 'Homepage was loaded successfully.');
     test.assertExists('form#user-login', 'Login form is present.');
   });
-  casper.thenOpen('/user/password', function() {
-  	test.assertHttpStatus(200, 'Request new password page was loaded successfully.');
+  casper.then(function(){
+    test.assertExists('header.clearfix'); // comprueba el header
+  });
+  casper.then(function(){
+    this.echo('Clicked in SOLICITAR UNA CONTRASEÑA')
+    casper.click("a[href='/user/password']"); //hacer click en un hipervinculo
+  });
+  casper.wait(1000, function() {});
+  casper.then(function(){
+    test.assertTitle('Solicitar una contraseña | AbbVie'); 
+    test.assertHttpStatus(200, 'Request new password page was loaded successfully.');
     test.assertExists('form#user-pass', 'Request new password form is present.');
   });
   casper.run(function() {
